47a55d380fe71a6cfb89098a9bc29d1da898b33c
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-04-06 08:48:42 -0500

    Deteccion de errores

--- FASTAPI-CURSO/S04_types/s04_01_autocompletado.py
+++ FASTAPI-CURSO/S04_types/s04_01_autocompletado.py
@@ -4,6 +4,3 @@ def get_full_name(first_name, last_name):
 
 print(get_full_name('carlos','Ramirez'))
 
-def get_full_name(first_name:str, last_name:str):
-  full_name = first_name.upper() + ' ' + last_name.upper()
-  return full_name
