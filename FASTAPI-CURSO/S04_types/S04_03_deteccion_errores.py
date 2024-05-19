3167f4c463ade2d474d133dac459be9d315f6d08
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-19 08:49:55 -0500

    Delete

--- FASTAPI-CURSO/S04_types/S04_03_deteccion_errores.py
+++
@@ -1,5 +0,0 @@
-def get_full_name(first_name:str, last_name:str):
-  full_name = first_name.upper() + ' ' + last_name.upper()
-  return full_name
-
-print(get_full_name('Carlos', 32))
--- FASTAPI-CURSO/S04_types/s04_01_autocompletado.py
+++
@@ -1,6 +0,0 @@
-def get_full_name(first_name, last_name):
-  full_name = first_name.capitalize() + ' ' + last_name.upper()
-  return full_name
-
-print(get_full_name('carlos','Ramirez'))
-
