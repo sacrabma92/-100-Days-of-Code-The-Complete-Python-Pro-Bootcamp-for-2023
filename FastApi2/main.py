adc80e8704ede8f039e30bc99e11af3e0287d0b1
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-19 20:39:50 -0500

    Put Modificando Datos

--- FastApi2/main.py
+++ FastApi2/main.py
@@ -1,5 +1,5 @@
 from fastapi.responses import HTMLResponse
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 #Crear una instancia de FastAPI
 app = FastAPI()
@@ -8,9 +8,9 @@ app.version = '0.0.1'
 
 # Ventas
 ventas = [
-    {'id': 1, 'nombre': 'Laptop', 'precio': 1000},
-    {'id': 2, 'nombre': 'Mouse', 'precio': 10},
-    {'id': 3, 'nombre': 'Teclado', 'precio': 20},
+    {'id': 1, 'nombre': 'Laptop', 'fecha': '31/01/2024', "tienda": 'TIENDA01', 'precio': 1000},
+    {'id': 2, 'nombre': 'Mouse', 'fecha': '06/02/2024', "tienda": 'TIENDA02', 'precio': 10},
+    {'id': 3, 'nombre': 'Teclado', 'fecha': '11/03/2024', "tienda": 'TIENDA03', 'precio': 20},
 ]
 
 # Crear punto de entreda
@@ -36,6 +36,36 @@ def venta_id(id: int):
 @app.get('/ventas/', tags=['Ventas'])
 def producto(nombre: str):
     return [elem for elem in ventas if elem['nombre'] == nombre]
+
+#Agregamos una nueva venta con el metodo POST y por medio de Body() toca importarla
+@app.post('/ventas', tags=['Ventas'])
+def agregar_venta(
+    id: int = Body(),
+    nombre: str = Body(),
+    fecha: str = Body(),
+    tienda: str = Body(),
+    precio: float = Body(),
+):
+  ventas.append({'id': id, 'nombre': nombre, 'fecha': fecha, 'tienda': tienda, 'precio': precio})
+  return {'mensaje': 'Venta agregada correctamente'}
+    
+@app.put('/ventas/{id}', tags=['Ventas'])
+def actualizar_venta(
+    id: int, 
+    nombre: str = Body(), 
+    fecha: str = Body(), 
+    tienda: str = Body(), 
+    precio: float = Body()
+    ):
+    for element in ventas:
+        if element['id'] == id:
+            element['nombre'] = nombre
+            element['fecha'] = fecha
+            element['tienda'] = tienda
+            element['precio'] = precio
+            return {'mensaje': 'Venta actualizada'}
+    return {'mensaje': 'Venta no encontrada'}
+
 
 
 
